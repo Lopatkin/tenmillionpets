30feac14bc3876be2d4ecf25a64b8071399c2254
Lopatkin <zedd218@gmail.com>
2024-08-05 14:27:36 +0300

    interface fix

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -77,7 +77,7 @@ const Chat = () => {
     const userFirstName = userData.first_name;
     const userLastName = userData.last_name;
     const userName = userData.username;
-    const userPhotoUrl = userData.photo_url;
+    const userPhotoUrl = userData.photo;
 
 
 

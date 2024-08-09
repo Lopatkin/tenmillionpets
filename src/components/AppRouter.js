6cde8b88b28467afe42b2cc62e8ea4e60aef95ac
Lopatkin <zedd218@gmail.com>
2024-08-09 12:53:37 +0300

    interface fix

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -21,9 +21,9 @@ const AppRouter = () => {
     const { userData } = useContext(Context);
     const { firestore } = useContext(Context)
 
-    const [isUserExist] = useCollectionData(
-        firestore.collection(fb_users).doc(userData.id)
-    )
+    // const [isUserExist] = useCollectionData(
+    //     firestore.collection(fb_users).doc(userData.id)
+    // )
 
     alert("appRoutes " + userData.id)
 

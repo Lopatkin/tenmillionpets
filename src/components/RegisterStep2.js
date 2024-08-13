3d834befefd8869f7761dc74f87cec3a8f0b0d26
Lopatkin <zedd218@gmail.com>
2024-08-13 16:42:38 +0300

    interface fix

--- src/components/RegisterStep2.js
+++ src/components/RegisterStep2.js
@@ -11,6 +11,7 @@ import { Pets } from '@mui/icons-material';
 
 const RegisterStep2 = () => {
 
+    var textValue;
     const { tg } = useContext(Context);
     tg.expand() // метод позволяет растянуть окно на всю высоту.
 
@@ -25,12 +26,15 @@ const RegisterStep2 = () => {
     };
 
     const human = () => {
-        alert('Human!');
+        document.getElementById("textRole").innerHTML = 'Хозяин';
+        document.getElementById("textRoleDescription").innerHTML = 'Вы сами принимаете за себя решение';
     }
 
     const pet = () => {
-        alert('Pet!');
+        document.getElementById("textRole").innerHTML = 'Питомец';
+        document.getElementById("textRoleDescription").innerHTML = 'Ваша судьба неопределена';
     }
+
 
     return (
 
@@ -49,24 +53,41 @@ const RegisterStep2 = () => {
 
                 <Grid container
                     justifyContent={'center'}
+
                 >
                     <div style={{
                         display: 'inline-block',
                         margin: '50px',
                         textAlign: 'center'
                     }}>
-
                         <img
                             onClick={human} width='20%' src='https://cdn.icon-icons.com/icons2/906/PNG/512/hand_icon-icons.com_70115.png' />
                         <img
                             onClick={pet} width='20%' src='https://cdn.promodj.com/afs/83a444d059de51cfcf3520e927a47e4a12%3Aresize%3A2000x2000%3Asame%3A269e3e' />
-
                     </div >
+
+                    <Grid container
+                        justifyContent={'center'}>
+
+                        <div style={{
+                            display: 'inline-block',
+                            width: '100%',
+                            textAlign: 'center'
+                        }} id="textRole" />
+
+                        <div style={{
+                            display: 'inline-block',
+                            width: '100%',
+                            textAlign: 'center'
+                        }} id="textRoleDescription" />
+                    </Grid>
+
 
 
                     <Button style={{
                         display: 'inline-block',
-                        width: '100%'
+                        width: '100%',
+                        marginTop: '20px'
 
                     }} onClick={navigateToRegisterStep3}>Выбрать</Button>
 

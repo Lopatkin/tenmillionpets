6a6aa5318d1fda656b3ba0ab930c7987604c8bd7
Lopatkin <zedd218@gmail.com>
2024-08-16 12:20:24 +0300

    interface fix

--- src/components/BufferPage.js
+++ src/components/BufferPage.js
@@ -3,6 +3,8 @@ import { Context } from '../index';
 import { useContext, Link } from 'react';
 import { Avatar, Button, Container, Grid, TextField } from '@mui/material';
 import { Route, Routes, useNavigate } from 'react-router-dom'
+import { fb_users, fb_messages } from '../utils/consts';
+
 
 import RegisterStep1 from "./RegisterStep1";
 
@@ -10,7 +12,30 @@ const BufferPage = () => {
     const { tg } = useContext(Context);
     tg.expand() // метод позволяет растянуть окно на всю высоту.
 
+    const { firestore } = useContext(Context);
+    const { userID } = useContext(Context);
+
+
     alert('это буффер')
+
+    var docRef = firestore.collection(fb_users).doc(userID);
+
+    docRef.get().then((doc) => {
+        if (doc.data().introPassed) {
+            alert('ок буффер ');
+
+            console.log("Document data:", doc.data());
+        } else {
+            alert('не ок буффер' + doc.data().introPassed);
+            //вот здесь начинается интро
+
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+
 
 
     const navigate = useNavigate();

e4f018a0a31934b7c6233de31948580c82b60d49
Lopatkin <zedd218@gmail.com>
2024-08-23 16:56:32 +0300

    interface fix

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -91,6 +91,9 @@ const AppRouter = () => {
 
                 <Route path="/introStep3" element={<IntroStep3 />} />
                 <Route path="*" element={<Navigate to="/introStep3" replace />} />
+
+                <Route path="/chat" element={<Chat />} />
+                <Route path="*" element={<Navigate to="/chat" replace />} />
             </Routes>
         )
     }

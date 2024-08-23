fc03f7980d7c859045ad4908b979f125ccbfba95
Lopatkin <zedd218@gmail.com>
2024-08-23 16:43:40 +0300

    interface fix

--- src/components/IntroStep3.js
+++ src/components/IntroStep3.js
@@ -11,6 +11,12 @@ import Chat from "./Chat";
 import { animalTypeVAR } from './RegisterStep2';
 import { animalType3 } from './IntroStep2';
 
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
+import { fb_users, fb_messages } from '../utils/consts';
+
 
 import { introStep3_human, introStep3_cat, introStep3_dog, animal_human, animal_cat, animal_dog } from '../utils/consts';
 
@@ -20,6 +26,12 @@ const IntroStep3 = (props) => {
     const animalTypeFB = doc.data()?.userAnimal;
     var animalType;
     var show_intro3;
+    const { userID } = useContext(Context);
+
+
+
+    const { firestore } = useContext(Context);
+
 
     if (animalTypeVAR != undefined) {
         animalType = animalTypeVAR;
@@ -51,10 +63,19 @@ const IntroStep3 = (props) => {
 
     const navigateToChat = () => {
         //Тут выставляем стартовые параметры
-        
+        firestore.collection(fb_users).doc(userID).set({
+            introPassed: true,
+            // home_id: '2323'
+        }).then(() => {
+            console.log("Document successfully written!");
+            // alert('Зарегились');
+            navigate('/chat');
+        })
+            .catch((error) => {
+                console.error("Error writing document: ", error);
+            });
 
 
-        navigate('/chat');
     };
 
     return (
